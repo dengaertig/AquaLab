d8cc22d7cd8f3316d3be431ad123673fa7bebc50
dengaertig <dennisgaertig@web.de>
2025-01-01 13:55:11 +0100

    loading .env

--- AquaLab/settings.py
+++ AquaLab/settings.py
@@ -11,19 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# .env-Datei laden
+load_dotenv()
 
+# Werte aus der .env-Datei abrufen
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-schlüssel')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@9+!$8eat073s0)96b1b%g%t4j6lsj!9t@f5b544tr_7-xh0fy'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
